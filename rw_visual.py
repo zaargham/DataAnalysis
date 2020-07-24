a47938a8e2fc31158e78ed7b420efb3349422dfb
zaargham <48851583+zaargham@users.noreply.github.com>
2020-07-24 23:19:11 +0530

    Add files via upload

--- rw_visual.py
+++ rw_visual.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while:
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+    plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
